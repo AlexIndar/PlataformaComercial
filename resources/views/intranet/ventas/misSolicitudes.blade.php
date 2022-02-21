6fde770cf2a3f726902cdeda8113b9bac14604fb
adanPrz7 <adan.perez@indar.com.mx>
2022-02-21 09:55:17 -0600

    msg alert was added for misSolicitudes

--- resources/views/intranet/ventas/misSolicitudes.blade.php
+++ resources/views/intranet/ventas/misSolicitudes.blade.php
@@ -206,19 +206,19 @@
                                             <div class="row mb-3">
                                                 <div class="col-md-4">
 
-                                                    <input type="text" name="RFC" id="rfcInput" placeholder="RFC" class="form-control" onfocusout="validaRFC()">
+                                                    <input type="text" name="RFC" id="rfcInput" placeholder="RFC" class="form-control" onfocusout="validaRFC()" maxlength="13">
                                                 </div>
                                                 <div class="col-md-4">
-                                                    <input type="text" name="RazonSocial" id="rzInput" placeholder="Nombre o razon social" class="form-control" onfocusout="validaRZI()">
+                                                    <input type="text" name="RazonSocial" id="rzInput" placeholder="Nombre o razon social" class="form-control" onfocusout="validaRZI()" maxlength="99">
                                                     <span>Apellido Paterno - Materno - Nombres(s)</span>
                                                 </div>
                                                 <div class="col-md-4">
-                                                    <input type="text" name="nombreComercial" id="nameComeInput" placeholder="Nombre Comercial" class="form-control" onfocusout="validaNameC()">
+                                                    <input type="text" name="nombreComercial" id="nameComeInput" placeholder="Nombre Comercial" class="form-control" onfocusout="validaNameC()" maxlength="99">
                                                 </div>
                                             </div>
                                             <div class="row mb-3">
                                                 <div class="col-md-4">
-                                                    <input type="text" name="numeroProspecto" id="prospecto" placeholder="Número de prospecto" class="form-control" onfocusout="validaProsP()">
+                                                    <input type="text" name="numeroProspecto" id="prospecto" placeholder="Número de prospecto" class="form-control" onfocusout="validaProsP()" maxlength="10">
                                                     <span>Capturar número de prospecto de la página web</span>
                                                 </div>
                                             </div>
@@ -275,13 +275,13 @@
                                             </div>
                                             <div class="row mb-3">
                                                 <div class="col-md-4">
-                                                    <input type="text" name="calle" id="calleInput" placeholder="Calle" class="form-control" onfocusout="validaCalle()">
+                                                    <input type="text" name="calle" id="calleInput" placeholder="Calle" class="form-control" onfocusout="validaCalle()" maxlength="99">
                                                 </div>
                                                 <div class="col-md-4">
-                                                    <input type="text" name="noExt" id="noExtInput" placeholder="No. Ext" class="form-control" onfocusout="validaNoEx()">
+                                                    <input type="text" name="noExt" id="noExtInput" placeholder="No. Ext" class="form-control" onfocusout="validaNoEx()" maxlength="10">
                                                 </div>
                                                 <div class="col-md-4">
-                                                    <input type="text" name="noInt" id="noIntInput" placeholder="No. Int" class="form-control" onfocusout="validaNoIn()">
+                                                    <input type="text" name="noInt" id="noIntInput" placeholder="No. Int" class="form-control" onfocusout="validaNoIn()" maxlength="10">
                                                 </div>
                                             </div>
                                             <div class="row mb-3">
@@ -289,7 +289,7 @@
                                                     <input type="text" name="codPos" id="cpInput" placeholder="C.P." class="form-control" onfocusout="updateGeolocation()" maxlength="5">
                                                 </div>
                                                 <div class="col-md-4">
-                                                    <input type="email" name="emailFacturacion" id="emailFac" placeholder="Correo@exmample.com" class="form-control" onfocusout="validaEmailF()">
+                                                    <input type="email" name="emailFacturacion" id="emailFac" placeholder="Correo@exmample.com" class="form-control" onfocusout="validaEmailF()" maxlength="49">
                                                     <span>Correo donde se enviará la factura</span>
                                                 </div>
                                                 <!-- <div class="col-md-4">
@@ -300,11 +300,11 @@
                                                 <div class="col-md-4" id="colDFRow1">
                                                     <span>Colonia</span>
                                                     <select id="colDF" name="colDF" class="form-control selectpicker" data-live-search="true">
-                                                    </select>                                                    
+                                                    </select>
                                                 </div>
                                                 <div class="col-md-4 d-none" id="colDFRow2">
                                                     <span>Colonia</span>
-                                                    <input type="text" name="auxColDF" id="auxColDF" placeholder="Ciudad" class="form-control" disabled>
+                                                    <input type="text" name="auxColDF" id="auxColDF" placeholder="Colonia" class="form-control" disabled>
                                                 </div>
                                                 <div class="col-md-4">
                                                     <span>Ciudad</span>
@@ -357,13 +357,13 @@
                                             <div class="shippingAddress" id="shippingAddress" style="display: none">
                                                 <div class="row mb-3">
                                                     <div class="col-md-4">
-                                                        <input type="text" name="calle" id="calleInputShipping" placeholder="Calle" class="form-control" onfocusout="validaCalleS()">
+                                                        <input type="text" name="calle" id="calleInputShipping" placeholder="Calle" class="form-control" onfocusout="validaCalleS()" maxlength="99">
                                                     </div>
                                                     <div class="col-md-4">
-                                                        <input type="text" name="noExt" id="noExtInputShipping" placeholder="No. Ext" class="form-control" onfocusout="validaNoExS()">
+                                                        <input type="text" name="noExt" id="noExtInputShipping" placeholder="No. Ext" class="form-control" onfocusout="validaNoExS()" maxlength="10">
                                                     </div>
                                                     <div class="col-md-4">
-                                                        <input type="text" name="noInt" id="noIntInputShipping" placeholder="No. Int" class="form-control" onfocusout="validaNoInS()">
+                                                        <input type="text" name="noInt" id="noIntInputShipping" placeholder="No. Int" class="form-control" onfocusout="validaNoInS()" maxlength="10">
                                                     </div>
                                                 </div>
                                                 <div class="row mb-3">
@@ -383,10 +383,14 @@
                                                     </div>
                                                 </div> -->
                                                 <div class="row mb-3">
-                                                    <div class="col-md-4">
+                                                    <div class="col-md-4" id="colDFRow3">
                                                         <span>Colonia</span>
                                                         <select id="colDFShipping" name="colDFShipping" class="form-control selectpicker" data-live-search="true">
                                                         </select>
+                                                    </div>
+                                                    <div class="col-md-4 d-none" id="colDFRow4">
+                                                        <span>Colonia</span>
+                                                        <input type="text" name="auxColDFShipping" id="auxColDFShipping" placeholder="Ciudad" class="form-control" disabled>
                                                     </div>
                                                     <div class="col-md-4">
                                                         <span>Ciudad</span>
@@ -419,7 +423,7 @@
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
-                                                    <input type="number" name="antiguedad" id="antiguedad" placeholder="Antigüedad" class="form-control" onfocusout="validaAnti()">
+                                                    <input type="number" name="antiguedad" id="antiguedad" placeholder="Antigüedad" class="form-control" onfocusout="validaAnti()" maxlength="4">
                                                 </div>
                                             </div>
                                             <div class="row mb-3 d-none" id="rowOtroGiro">
@@ -487,7 +491,7 @@
                                                         </select>
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <input type="text" name="nombreContacto" id="nombreContacto" placeholder="Nombre" class="form-control" onfocusout="validateNameC()">
+                                                        <input type="text" name="nombreContacto" id="nombreContacto" placeholder="Nombre" class="form-control" onfocusout="validateNameC()" maxlength="49">
                                                     </div>
                                                     <div class="input-group mb-3">
                                                         <input type="text" name="telefonoContacto" id="telefonoContacto" placeholder="Telefono" class="form-control" maxlength="10">
@@ -496,7 +500,7 @@
                                                         <input type="text" name="celularContacto" id="celularContacto" placeholder="Celular" class="form-control" maxlength="10">
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <input type="email" name="emailContacto" id="emailContacto" placeholder="Correo" class="form-control">
+                                                        <input type="email" name="emailContacto" id="emailContacto" placeholder="Correo" class="form-control" maxlength="49">
                                                     </div>
                                                     <div class="input-group mb-3 checkForm">
                                                         <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
@@ -728,13 +732,13 @@
                                             <div class="row" id="refGroup" style="display: none;">
                                                 <div class="col-md-4">
                                                     <div class="input-group mb-3">
-                                                        <input type="text" name="razonSocialRef" id="razonSocialRef" placeholder="Razón Social" class="form-control">
+                                                        <input type="text" name="razonSocialRef" id="razonSocialRef" placeholder="Razón Social" class="form-control" maxlength="49">
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <input type="text" name="contactoRef" id="contactoRef" placeholder="Contacto" class="form-control">
+                                                        <input type="text" name="contactoRef" id="contactoRef" placeholder="Contacto" class="form-control" maxlength="20">
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <input type="text" name="ciudadRef" id="ciudadRef" placeholder="Ciudad" class="form-control">
+                                                        <input type="text" name="ciudadRef" id="ciudadRef" placeholder="Ciudad" class="form-control" maxlength="49">
                                                     </div>
                                                     <div class="input-group mb-3">
                                                         <input type="text" name="telefonoRef" id="telefonoRef" placeholder="Telefono" class="form-control" maxlength="10">
@@ -938,6 +942,19 @@
                             <div class="col-md-4">Fotografia de Solicitud</div>
                             <div class="col-md-6" id="imgFSButton"></div>
                             <div class="col-md-2" id="picSolButtons">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-4">Fotografia de Solicitud</div>
+                            <div class="col-md-6">
+                                <div class="input-group input-group-sm">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="editPictureSol" accept="image/x-png,image/gif,image/jpeg">
+                                        <label class="custom-file-label" for="editPictureSol" id="label-editPictureSol">Seleccionar Archivo...</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2" id="XXXX2">
                             </div>
                         </div>
                         <div class="row mb-3">
@@ -1305,6 +1322,21 @@
     </div>
 </div>
 
+
+<div class="modal" tabindex="-1" id="alertModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-danger">
+                <h5 class="modal-title" id="titleModalR"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Alerta del formulario <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center" id="alertInfoModal">
+            </div>
+        </div>
+    </div>
+</div>
 <!-- VALIDATE DATA MODAL -->
 <!-- <div class="modal-background" id="validateModal">
     <div class="modal-header bg-indarBlue">
