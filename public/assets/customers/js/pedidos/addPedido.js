1355377b75e0ad7bdb05ac95e6a10ceecc7cba6d
AlexIdar <alejandro.jimenez@indar.com.mx>
2022-05-02 17:23:57 -0500

    excel readonly first sheet

--- public/assets/customers/js/pedidos/addPedido.js
+++ public/assets/customers/js/pedidos/addPedido.js
@@ -135,13 +135,9 @@ $(document).ready(function() {
         reader.onload = function() {
             var fileData = reader.result;
             var wb = XLSX.read(fileData, { type: 'binary' });
-
-            wb.SheetNames.forEach(function(sheetName) {
-                var rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
-                var jsonObj = JSON.stringify(rowObj);
-                cargarProductosExcel(jsonObj);
-
-            })
+            var rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets[wb['SheetNames'][0]]);
+            var jsonObj = JSON.stringify(rowObj);
+            cargarProductosExcel(jsonObj);
         };
         reader.readAsBinaryString(input.files[0]);
     });
