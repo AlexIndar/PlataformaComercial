baf42abcd0d3a13aab4101e0d4c891560a54564b
Desarrollo <antonio.aguilar@indar.com.mx>
2022-05-25 09:45:39 -0500

    cambios global api

--- app/Http/Controllers/Comisiones/ComisionesController.php
+++ app/Http/Controllers/Comisiones/ComisionesController.php
@@ -10,24 +10,24 @@ use App\Http\Controllers\Customer\TokenController;
 class ComisionesController extends Controller
 {
     public static function getInfoCobranzaZonaWeb($token, $referencia,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getInfoCobranzaZonaWeb?referencia='.$referencia.'&fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getInfoCobranzaZonaWeb?referencia='.$referencia.'&fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function getDiasNoHabiles($token, $zona,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getDiasNoHabiles?zona='.$zona.'&fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getDiasNoHabiles?zona='.$zona.'&fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function postParametroCtesZona($token, $referencia,$parametroCte){
-        $data = Http::withToken($token)->post('http://192.168.70.107:64444/CobranzaZona/postParametroCtesZona?referencia='.$referencia.'&parametroCte='.$parametroCte);
+        $data = Http::withToken($token)->post(config('global.api_url').'/CobranzaZona/postParametroCtesZona?referencia='.$referencia.'&parametroCte='.$parametroCte);
         return json_decode($data->body());
     }
 
     public static function  postActualizarArticulosEspeciales($token, $json){
         //dd(json_encode($json));
         $json = json_decode($json);
-        $data = Http::withToken($token)->post('http://192.168.70.107:64444/Especiales/postActualizarArticulosEspeciales',[
+        $data = Http::withToken($token)->post(config('global.api_url').'/Especiales/postActualizarArticulosEspeciales',[
             "ArtEspeciales" => $json
         ]);
 
@@ -37,7 +37,7 @@ class ComisionesController extends Controller
     public static function  postActualizarEspeciales($token, $json){
         $json = json_decode($json);
         //dd($json);
-        $data = Http::withToken($token)->post('http://192.168.70.107:64444/Especiales/postActualizarEspeciales',[
+        $data = Http::withToken($token)->post(config('global.api_url').'/Especiales/postActualizarEspeciales',[
             "EspecialesModel" => $json
         ]);
 
@@ -45,32 +45,32 @@ class ComisionesController extends Controller
     }
 
     public static function getEspecialesPorPeriodo($token, $year,$month){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/Especiales/getEspecialesPorPeriodo?year='.$year.'&month='.$month);
+        $data = Http::withToken($token)->get(config('global.api_url').'/getEspecialesPorPeriodo?year='.$year.'&month='.$month);
         return json_decode($data->body());
     }
 
     public static function getCtesActivosMes($token, $referencia,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getCtesActivosMes?referencia='.$referencia.'&fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getCtesActivosMes?referencia='.$referencia.'&fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function getTotalVentasZona($token, $referencia,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getTotalVentasZona?zona='.$referencia.'&fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getTotalVentasZona?zona='.$referencia.'&fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function getHistoricoCobranzaZonaList($token,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getHistoricoCobranzaZonaList?fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getHistoricoCobranzaZonaList?fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function getExistePeriodoEjercicio($token,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getExistePeriodoEjercicio?fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getExistePeriodoEjercicio?fecha='.$fecha);
         return json_decode($data->body());
     }
 
     public static function getCierreMesCobranzaZona($token,$fecha){
-        $data = Http::withToken($token)->get('http://192.168.70.107:64444/CobranzaZona/getCierreMesCobranzaZona?fecha='.$fecha);
+        $data = Http::withToken($token)->get(config('global.api_url').'/CobranzaZona/getCierreMesCobranzaZona?fecha='.$fecha);
         return json_decode($data->body());
     }
 
