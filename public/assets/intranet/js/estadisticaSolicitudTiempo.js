a7895b11ba197e986f895c500dfd1044d0bd77ae
adanPrz7 <adan.perez@indar.com.mx>
2022-06-07 13:08:24 -0500

    estadisticaTiempo

--- public/assets/intranet/js/estadisticaSolicitudTiempo.js
+++ public/assets/intranet/js/estadisticaSolicitudTiempo.js
@@ -1,4 +1,30 @@
+var idGerencia = '';
+
+var startDate = '';
+var endDate = '';
 $(document).ready(function() {
+    if(document.getElementById("userR").value == "GERENTEVENTA"){
+        let data = { User: document.getElementById("userP").value };
+        $.ajax({
+            'headers': {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            'url': "/EstadisticaSolicitudTiempo/GetGerencia",
+            'type': 'POST',
+            'dataType': 'json',
+            'data': data,
+            'enctype': 'multipart/form-data',
+            'timeout': 2 * 60 * 60 * 1000,
+            success: function(report) {
+                idGerencia = report.id;
+            },
+            error: function(error) {
+                idGerencia = '';
+                console.log(error);
+            }
+        });
+    }
+
     $('input[name="daterange"]').daterangepicker({
         opens: 'right',
         autoUpdateInput: true,
@@ -34,8 +60,26 @@ $(document).ready(function() {
         // console.log(start.format('YYYY-MM-DD'));
         // console.log(end.format('YYYY-MM-DD'));
         getEstadisticaTiempo();
+        // console.log("SI");
+        // console.log("Prueb");
         // startDate = start.format('YYYY-MM-DD');
         // endDate = end.format('YYYY-MM-DD');
+    });
+
+    $(function() {
+        $("#tableCyc").DataTable({
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "paging": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "buttons": ["csv", "excel", "pdf"],
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+            },
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
 });
 
@@ -43,18 +87,145 @@ document.getElementById("typeForms").addEventListener("change", function(e) {
     getEstadisticaTiempo();
 });
 
-
-
 const getEstadisticaTiempo = () => {
+    console.log(startDate);
+    console.log(endDate);
     var fecha = $('#reservation').daterangepicker()[0].value;
     var dateIF = fecha.split('-').map(s => s.trim());
     let typeFol = document.getElementById("typeForms").value;
-    console.log(typeFol);
-    console.log(dateIF[0]);
-    console.log(dateIF[1]);
+    if(typeFol != "SELECCIONAR"){
+        if(document.getElementById("userR").value != "GERENTEVENTA"){
+            getTimeReport(typeFol, dateIF[0], dateIF[1]);
+        }else{
+            getManagementTimeReport(idGerencia, typeFol, dateIF[0], dateIF[1]);
+        }
+    }else{
+        alert("Selecciona un tipo de formulario");
+    }
     document.getElementById("exportFileBtn").disabled = false;
 }
 
-const alterb = () =>{
-    alert("HOLA");
+const getTimeReport = (typeRequest, ini, end) =>{
+    let jsonTimeReport ={
+        TypeR: typeRequest,
+        Ini: ini,
+        End: end
+    }
+    $.ajax({
+        'headers': {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        'url': "/EstadisticaSolicitudTiempo/GetTimeReport",
+        'type': 'POST',
+        'dataType': 'json',
+        'data': jsonTimeReport,
+        'enctype': 'multipart/form-data',
+        'timeout': 2 * 60 * 60 * 1000,
+        success: function(response) {
+            cargarTableCyc(response);
+        },
+        error: function(error) {            
+            console.log(error);
+        }
+    });
+}
+
+const getManagementTimeReport = (idArea, typeRequest, ini, end) =>{
+    let jsonTimeReport ={
+        IdArea: idArea,
+        TypeR: typeRequest,
+        Ini: ini,
+        End: end
+    }
+    $.ajax({
+        'headers': {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        'url': "/EstadisticaSolicitudTiempo/GetManagementTimeReport",
+        'type': 'POST',
+        'dataType': 'json',
+        'data': jsonTimeReport,
+        'enctype': 'multipart/form-data',
+        'timeout': 2 * 60 * 60 * 1000,
+        success: function(report) {
+            console.log(report);
+        },
+        error: function(error) {            
+            console.log(error);
+        }
+    });
+}
+
+const cargarTableCyc = (solicitudesList) =>{
+    let dataTableCyc = [];
+    for(let i = 0; i < solicitudesList.length; i++){
+        let auxList = [];
+        auxList.push(solicitudesList[i].businessName);
+        auxList.push(solicitudesList[i].zone);
+        auxList.push(dateTimeFilter(solicitudesList[i].total));
+        auxList.push(dateTimeFilter(solicitudesList[i].seller));
+        auxList.push(dateTimeFilter(solicitudesList[i].revision));
+        auxList.push(dateTimeFilter(solicitudesList[i].references));
+        auxList.push(dateTimeFilter(solicitudesList[i].authorization));
+        auxList.push(solicitudesList[i].status);
+        auxList.push(dateFilter(solicitudesList[i].date));
+        if(solicitudesList[i].status == "Rechazada")
+            auxList.push(`<button class="btn btn-danger btn-circle float-right"><i class="fas fa-times"></i></button>`);
+        else
+            auxList.push(`<button class="btn btn-success btn-circle float-right"><i class="fas fa-check"></i></button>`);
+        // dateFilter(solicitudesList[i].date);
+        // auxList.push(solicitudesList[i].dateAltaIntelisis);
+        // auxList.push(solicitudesList[i].datePCompra);
+        // auxList.push(solicitudesList[i].importePCompra);
+        dataTableCyc.push(auxList);
+    }
+    $("#tableCyc").dataTable().fnClearTable();
+    $("#tableCyc").dataTable().fnDraw();
+    $("#tableCyc").dataTable().fnDestroy();
+    // console.log(dataTableCyc);
+    $("#tableCyc").DataTable({
+        "data": dataTableCyc,        
+        "responsive": true,
+        "lengthChange": true,
+        "autoWidth": false,
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        },
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+}
+
+const dateTimeFilter = (time) =>{
+    // set minutes to seconds
+    let seconds = time * 60
+    // calculate (and subtract) whole days
+    let days = Math.floor(seconds / 86400);
+    seconds -= days * 86400;
+    // calculate (and subtract) whole hours
+    let hours = Math.floor(seconds / 3600) % 24;
+    seconds -= hours * 3600;
+    // calculate (and subtract) whole minutes
+    let minutes = Math.floor(seconds / 60) % 60;
+    let d = days > 0 ? days + 'd ' : '';
+    let h = hours > 0 ? hours + 'h ' : '';
+    let m = minutes > 0 ? minutes + 'm ' : '';
+    let result = d + h + m;
+    return  result == '' ? '-' : result;
+}
+
+const dateFilter = (date) =>{
+    let dateIF = date.split('T').map(s => s.trim());
+    let aux1 = dateIF[0].split('-').map(s => s.trim());
+    let aux2 = dateIF[1].split(':').map(s => s.trim());
+    return aux1[2] + "/" + aux1[1] + "/" + aux1[0] + " " + timeFilter(aux2);    
+}
+
+const timeFilter = (time) =>{
+    let one = time[0] > 12 ? time[0] - 12 : time[0];
+    let ls = time[0] > 12 ? " pm" : " am";
+    return one + ":" + time[1] + ":" + time[2].split('.').map(s => s.trim())[0] + ls;
 }
