96016542a10ea25cd35e0282ac2c86ef32eae473
adanPrz7 <adan.perez@indar.com.mx>
2022-06-09 15:30:15 -0500

    s

--- config/global.php
+++ config/global.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'api_url' => 'http://192.168.70.107:64444'    
+    'api_url' => 'http://192.168.70.107:64444'
+    // 'api_url' => 'https://localhost:44384/'
     //'api_url' => 'http://192.168.70.107:8089'
 ]
 
